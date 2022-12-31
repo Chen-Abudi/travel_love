317a6f03c424050c55620ea0c8444618dcb2a5f1
Chen-Abudi <cheezumcali23@gmail.com>
2022-12-31 21:15:40 +0200

    dev: footer section, scroll top and bar, light/dark mode adjustment

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ if (navToggle) {
     navMenu.classList.add("show-menu");
   });
 }
-
 // Menu Hidden
 // Validate if const existing
 if (navClose) {
@@ -64,20 +63,17 @@ function playPause() {
   if (videoFile.paused) {
     // Play the video
     videoFile.play();
-
     // Changing the Icon here
     videoIcon.classList.add("ri-pause-line");
     videoIcon.classList.remove("ri-play-line");
   } else {
     // Pause the video
     videoFile.pause();
-
     // Changing the Icon here
     videoIcon.classList.remove("ri-pause-line");
     videoIcon.classList.add("ri-play-line");
   }
 }
-
 videoButton.addEventListener("click", playPause);
 
 function finiteVideo() {
@@ -85,6 +81,77 @@ function finiteVideo() {
   videoIcon.classList.remove("ri-pause-line");
   videoIcon.classList.add("ri-play-line");
 }
-
 // Where/when video's ending
 videoFile.addEventListener("ended", finiteVideo);
+
+// ─── Reveal Scrollup ──────────────────────────────────────────────
+function scrollToTop() {
+  const scrollToTop = document.getElementById("scroll-top");
+
+  /* If the scroll is higher or equal to 220 
+  viewport height, than add the scroll */
+  if (this.scrollY >= 280) {
+    scrollToTop.classList.add("reveal-scroll");
+  } else {
+    scrollToTop.classList.remove("reveal-scroll");
+  }
+}
+window.addEventListener("scroll", scrollToTop);
+
+// ─── Active Link Scrollup Sections ────────────────────────────────
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("activate-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("activate-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+// ─── Light/Dark Mode ───────────────────────────────────────────────
+const modeButton = document.getElementById("mode-button");
+const darkMode = "dark-mode";
+const iconMode = "ri-sun-line";
+
+const chosenMode = localStorage.getItem("chosen-mode");
+const chosenIcon = localStorage.getItem("chosen-icon");
+
+const getCurrentMode = () =>
+  document.body.classList.contains(darkMode) ? "dark" : "light";
+
+const getCurrentIcon = () =>
+  modeButton.classList.contains(iconMode) ? "ri-moon-line" : "ri-sun-line";
+
+// Checking if the user has chose the mode already before
+if (chosenMode) {
+  /* If it's validate, will check if the dark mode was activated
+  or deactivated */
+  document.body.classList[chosenMode === "dark" ? "add" : "remove"](darkMode);
+  modeButton.classList[chosenIcon === "ri-moon-line" ? "add" : "remove"](
+    iconMode
+  );
+}
+
+// Manually Activate or Deactivate mode with its button
+modeButton.addEventListener("click", () => {
+  // Adding or removing the dark-icon mode
+  document.body.classList.toggle(darkMode);
+  modeButton.classList.toggle(iconMode);
+  // Saving here the user's chosen mode and its icon
+  localStorage.setItem("chosen-mode", getCurrentMode());
+  localStorage.setItem("chosen-icon", getCurrentIcon());
+});
